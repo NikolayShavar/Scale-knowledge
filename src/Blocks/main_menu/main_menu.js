1b390d90d2892a34bad32a39948ee2b0c1d55041
Nikolay <121959603+NikolayShavar@users.noreply.github.com>
2024-02-29 18:34:52 +0200

    Delete src directory

--- src/Blocks/main_menu/main_menu.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("burger").addEventListener("click", function () {
-      document.querySelector(".menu_btns").classList.toggle("open");
-    });
-  });
